bf99a66b9c24422dfd274accdceb15c8116bf40b
ZeArioch <16936254+ZeArioch@users.noreply.github.com>
2020-06-02 19:03:03 +0200

    proofread README + python usage

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # etwbreaker
-An IDA Plugin to find statically ETW events into a PE and generate a Conditional Breakpoint for facilitate Security Research.
+An IDA Plugin to statically find ETW events in a PE file and generate a Conditional Breakpoint to facilitate Security Research.
 
 ![Demo](img/demo.gif)
 
-## How To Install ?
+## How To Install?
 
-Just put the `etwbreaker.py` script in `plugins` folder of IDA.
+Just put the `etwbreaker.py` script in the `plugins` folder of IDA.
 
 ```
 git clone git@github.com:Airbus-CERT/etwbreaker.git
@@ -14,25 +14,24 @@ mklink "C:\\Program Files\\IDA Pro 7.4\\plugins\\etwbreaker.py" "etwbreaker\etwb
 
 Launch your IDA and press `Ctrl-Shift-L` to activate it.
 
-## How Does It Works ?
+## How Does It Work?
 
 `ETWBreaker` try to find all references about ETW providers statically compiled into a Windows module.
 
-### Manifest based Provider
+### Manifest-based Provider
 
-`ETWBreaker` will try to find a ressource names `WEVT_TEMPLATE`. This ressource include the ETW manifest for the module.
-Once we get all events availables, we can compute a signature and try to find associated symbol of the event to enrich analysis.
+`ETWBreaker` will try to find a resource name `WEVT_TEMPLATE`. This resource includes the ETW manifest for the module.
+Once we get all events available, we can compute a signature and try to find the associated symbol of the event to enrich analysis.
 Then we can also generate a conditional breakpoint to debug the module only once the target event is triggered.
-
 
 ### Tracelogging provider
 
-`Microsoft` recently add `Tracelogging` API, that is work over ETW but without manifest. 
-Tracelogging encompass it's scheme directly into a special field of ETW name `ExtendedData`. 
-Tracelogging API is a macro based API, it's means that scheme are generated during compilation step and can be retrieve statically.
-Scheme data are contain in a bordered region, for security purpose, and can be retrieve very easily.
+`Microsoft` recently added the `Tracelogging` API, that works over ETW but without manifests.
+Tracelogging encompasses its scheme directly into a special ETW field named `ExtendedData`.
+The Tracelogging API is a macro-based API, it means that schemes are generated at compilation and can be retrieved statically.
+Scheme data are contained in a bordered region for security purposes, and can be retrieved easily.
 
-But, in oposite of manifest ETW, the link between event and provider are made at execution time, and all event have same id(0).
+But, to the contrary of manifest-based ETW, the link between event and provider is made at execution time, and all events have the same ID (0).
 This is why we list only providers in case of Tracelogging.
 
 ## SSTIC (Symposium sur la sécurité des technologies de l'information et des communications)
--- etwbreaker.py
+++ etwbreaker.py
@@ -1,14 +1,14 @@
 """
 ETWBreaker is an IDA plugin that find all references about ETW into a module
 
-Actually ETWBreaker can work with Manifest based ETW and Tracelogging.
+Currently ETWBreaker can work with Manifest-based ETW and Tracelogging.
 
-For Manifest based ETW, ETWBreaker will parse module ressources to find and parse the manifest
-and show all possible event handled by the current module.
+For Manifest-based ETW, ETWBreaker will parse module ressources to find and parse the manifest
+and show all possible events handled by the current module.
 
-It try to parse also all tracelogging event, which are more hidden than Manifest.
+It tries to parse also all tracelogging events, which are more hidden than Manifest-based ones.
 
-At the end ETWBreaker can generate a conditional breakpoint to dynamically analyse event.
+In the end ETWBreaker can generate a conditional breakpoint to dynamically analyze events.
 """
 
 import idaapi
@@ -33,7 +33,7 @@ class ETWBreakerException(Exception):
     def __init__(self, message):
         super().__init__(message)
 
- 
+
 class ETWBreakerWevtTemplateNotFound(ETWBreakerException):
     """
     The WEVT_TEMPLATE ressource was not found
@@ -41,7 +41,7 @@ class ETWBreakerWevtTemplateNotFound(ETWBreakerException):
     def __init__(self):
         super().__init__("WEVT_TEMPLATE resource not found.")
 
-       
+
 class ETWBreakerTLNotFound(ETWBreakerException):
     """
     The tracelogging magic was not found
@@ -57,21 +57,21 @@ class ETWBreakerUnexpectedToken(ETWBreakerException):
     """
     def __init__(self, expected, found):
         super().__init__("Unexpected token. Expected %s, found %s"%(expected, found))
-        
-        
+
+
 class Stream(BytesIO):
     """
     A wrapper that is nicer to understand
     """
     def read_u32(self) -> int:
         return struct.unpack("<I", self.read(4))[0]
-        
+
     def read_u16(self) -> int:
         return struct.unpack("<H", self.read(2))[0]
-        
+
     def read_u8(self) -> int:
         return struct.unpack("<B", self.read(1))[0]
-        
+
     def read_u64(self) -> int:
         return struct.unpack("<Q", self.read(8))[0]
 
@@ -82,7 +82,7 @@ class Guid:
     """
     def __init__(self, raw):
         self.raw = raw
-        
+
     def __str__(self):
         Data1 = struct.unpack("<I", self.raw[0:4])[0]
         Data2 = struct.unpack("<H", self.raw[4:6])[0]
@@ -90,7 +90,7 @@ class Guid:
         Data4 = self.raw[8:16]
         return "%08x-%04x-%04x-%s-%s" % (Data1, Data2, Data3, "".join("%02x" % x for x in Data4[0:2]), "".join("%02x" % x for x in Data4[2:]))
 
-    
+
 
 class ETWBreaker(idaapi.ida_idaapi.plugin_t):
     """
@@ -119,7 +119,7 @@ class ETWBreaker(idaapi.ida_idaapi.plugin_t):
         """
         ETWBreaker.log("%s is enabled now." % ETWBreaker.wanted_name)
         providers = []
-        
+
         # Manifest based provider
         try:
             providers += parse_manifest(find_wevt_template(*find_segment(".rsrc")[0]))
@@ -127,20 +127,20 @@ class ETWBreaker(idaapi.ida_idaapi.plugin_t):
             ETWBreaker.log("Please concider to reload the file and check the 'Load Resource' checkbox")
         except ETWBreakerException as e:
             ETWBreaker.log(str(e))
-        
+
         # Tracelogging
         for segment in find_segment(".rdata"):
-            try:    
+            try:
                 providers += parse_tracelogging(find_tracelogging_meta(*segment))
             except ETWBreakerException as e:
                 ETWBreaker.log(str(e))
-        
+
         ETWResultsForm(providers).show()
-        
+
 
     def term(self):
         ETWBreaker.log("%s unloaded." % (ETWBreaker.wanted_name))
-            
+
     def log(message):
         idaapi.msg("[%s] %s\n" % (ETWBreaker.wanted_name, message))
 
@@ -157,11 +157,11 @@ class Event:
         self.opcode = opcode
         self.task = task
         self.keyword = keyword
-        
+
     def find_symbol(self) -> str:
         """
         Try to find a symbol associated to the event
-        
+
         This is based on the event header signature
         Most of then are into .rdata segment and some of them have a name
         """
@@ -170,11 +170,11 @@ class Event:
             offset = idc.get_bytes(start, end - start).find(pattern)
             if offset == -1:
                 continue
-                
+
             name = ida_name.get_name(start+offset)
             if name is None:
                 continue
-            
+
             return name
         return None
 
@@ -190,10 +190,10 @@ class Channel:
         """
         self.identifier = identifier
         self.name = name
-        
+
     def __str__(self):
         return self.name
- 
+
 
 class Provider:
     """
@@ -209,14 +209,14 @@ class Provider:
         self.guid = guid
         self.events = events
         self.channels = channels
-        
+
     def find_channel(self, identifier: int) -> Channel:
         """
         Try to find a channel from its identifier
         """
         return next((channel for channel in self.channels if channel.identifier == identifier), None)
 
- 
+
 class ManifestProvider(Provider):
     """
     Convenient class use to identify Manifest based providers
@@ -232,7 +232,7 @@ class TraceLoggingProvider(Provider):
 def find_segment(name: str) -> List[Tuple[int, int]]:
     """
     Try to find the segment from name
-    
+
     :ivar name: name of segment
     :ret: Start ant end address
     """
@@ -242,12 +242,12 @@ def find_segment(name: str) -> List[Tuple[int, int]]:
             result.append((idc.get_segm_start(seg), idc.get_segm_end(seg)))
     return result
 
-    
+
 def find_wevt_template(start, end) -> Stream:
     """
     This function try to retrieve the WEVT_TEMPLETE resource
     This resource start with the magic CRIM
-    
+
     :ivar start: start address
     :ivar end: end address
     :ret: Stream use to parse Manifest based provider or raise an exception
@@ -257,14 +257,14 @@ def find_wevt_template(start, end) -> Stream:
     result = resource.find(b"CRIM")
     if result == -1:
         raise ETWBreakerWevtTemplateNotFound()
-       
+
     return Stream(resource[result:])
-    
+
 
 def find_tracelogging_meta(start, end) -> Stream:
     """
     Try to find ETW0 magic
-    
+
     :ivar start: start address
     :ivar end: end address
     :ret: Stream use to parse tracelogging or None if not found
@@ -273,7 +273,7 @@ def find_tracelogging_meta(start, end) -> Stream:
     result = data.find(b"ETW0")
     if result == -1:
         raise ETWBreakerTLNotFound()
-        
+
     return Stream(data[result:])
 
 
@@ -282,7 +282,7 @@ def parse_tracelogging_event(stream: Stream) -> Event:
     A tracelogging event is identified by its channel number_of_channel
     that are always 11. Actually we can't handle tracelogging event
     because the lonk between event and provider is made during code execution
-    
+
     :ivar stream: current stream use to parse the event
     :ret: An event object for tracelogging
     """
@@ -302,7 +302,7 @@ def parse_tracelogging_provider(stream: Stream) -> Provider:
     Create a default provider for tracelogging
     It will add a default event for this provider
     Because in traclogging all event have the event id set to 0
-    
+
     :ivar stream: current stream use to parse the provider
     :ret: A provider object for tracelogging
     """
@@ -310,7 +310,7 @@ def parse_tracelogging_provider(stream: Stream) -> Provider:
     size = stream.read_u16()
     payload = stream.read(size - 2)
     name = payload[:payload.find(b"\x00")].decode("ascii")
-    
+
     return TraceLoggingProvider(guid, [Event(0, 0, 11, 0, 0, 0, 0)], [Channel(11, name)])
 
 
@@ -318,17 +318,17 @@ def parse_tracelogging(stream: Stream) -> List[Provider]:
     """
     Try to parse all tracelogging event and provider
     from an .rdata segmant
-    
+
     Actually only provider are intersting. It's because the link
     between event and provider are made into the code dynamically.
-    
+
     :ivar stream: current stream use to parse the event
     :ret: the list of all provider which are found
     """
     magic = stream.read(4)
     if magic != b"ETW0":
         raise ETWBreakerUnexpectedToken(b"ETW0", magic)
-        
+
     stream.read(12)
     providers = []
     while True:
@@ -354,13 +354,13 @@ def parse_event_elements(stream: Stream) -> List[Event]:
     * a channel
     * a set of keywords
     * a level
-    
+
     :ivar stream: Input stream once read the EVNT magic and the size of the payload
     :ret: List of all event parsed
     """
     number_of_event = stream.read_u32()
     stream.read(4) # padding
-    
+
     events = []
     for i in range(0, number_of_event):
         event_id = stream.read_u16()
@@ -379,7 +379,7 @@ def parse_event_elements(stream: Stream) -> List[Event]:
         events.append(Event(event_id, version, channel, level, opcode, task, keywords))
     return events
 
- 
+
 def parse_channel_element(stream: Stream) -> List[Channel] :
     number_of_channel = stream.read_u32()
     result = []
@@ -395,45 +395,45 @@ def parse_channel_element(stream: Stream) -> List[Channel] :
         name = sub_stream.read(size-4).decode("utf-16le")
         result.append(Channel(identifier, name))
     return result
-         
+
 
 def parse_event_provider(guid: Guid, stream: Stream) -> Provider:
     """
     Parse an event provider
     An event provider is composed by a plenty of sort of element:
     * EVNT for event
-    
+
     https://github.com/libyal/libfwevt/blob/master/libfwevt/fwevt_template.h
-    
+
     :ivar guid: GUID of the provider
     :ivar stream: stream of the entire resource with offset set to the start of the provider
     """
     magic = stream.read(4)
     if magic != b"WEVT":
         raise ETWBreakerUnexpectedToken(b"WEVT", magic)
-        
+
     size = stream.read_u32()
     message_table_id = stream.read_u32()
-    
+
     number_of_element = stream.read_u32()
     number_of_unknown = stream.read_u32()
-    
+
     element_descriptor = [(stream.read_u32(), stream.read_u32()) for i in range(0, number_of_element)]
     unknown = [stream.read_u32() for i in range(0, number_of_unknown)]
-    
+
     events = []
     channels = []
     for offset, _ in element_descriptor:
         stream.seek(offset)
         magic = stream.read(4)
         size = stream.read_u32()
-        
+
         # Event declaration
         if magic == b"EVNT":
             events = parse_event_elements(stream)
         elif magic == b"CHAN":
             channels = parse_channel_element(stream)
-    
+
     return ManifestProvider(guid, events, channels)
 
 
@@ -441,33 +441,33 @@ def parse_manifest(stream: Stream) -> List[Provider]:
     """
     An ETW Manifest is a binary serialized
     It start with CRIM magic
-    
+
     Then list all providers
     For each providers we can parse GUID and Provider description
-    
+
     """
     magic = stream.read(4)
     if magic != b"CRIM":
         raise ETWBreakerUnexpectedToken(b"CRIM", magic)
-        
+
     size = stream.read_u32()
-    
+
     major_version = stream.read_u16()
     minor_version = stream.read_u16()
-    
+
     number_of_provider_descriptor = stream.read_u32()
-    
+
     # Read provider meta informations
     providers_descriptor = [(Guid(stream.read(16)), stream.read_u32()) for i in range(0, number_of_provider_descriptor)]
-    
+
     # Parse providers
     providers = []
     for guid, offset in providers_descriptor:
         stream.seek(offset)
         providers.append(parse_event_provider(guid, stream))
-        
+
     return providers
-    
+
 
 def add_breakpoint(guid: Guid, event: Event):
     """
@@ -504,7 +504,7 @@ def delete_breakpoint(symbol: str):
     """
     location = idaapi.bpt_location_t()
     location.set_sym_bpt(symbol)
-    
+
     if idaapi.find_bpt(location, None):
         idaapi.del_bpt(location)
 
@@ -518,8 +518,8 @@ class ETWResultsModel(QtCore.QAbstractTableModel):
     COL_GUID = 0x02
     COL_CHANNEL = 0x03
     COL_SYMBOL = 0x04
-    
-        
+
+
     def __init__(self, providers: List[Provider], parent=None):
         super().__init__(parent)
 
@@ -527,14 +527,14 @@ class ETWResultsModel(QtCore.QAbstractTableModel):
             ETWResultsModel.COL_ID : 'Event ID',
             ETWResultsModel.COL_TYPE : 'Type',
             ETWResultsModel.COL_GUID : 'GUID',
-            ETWResultsModel.COL_CHANNEL : 'Channel',           
-            ETWResultsModel.COL_SYMBOL : 'Symbol'            
+            ETWResultsModel.COL_CHANNEL : 'Channel',
+            ETWResultsModel.COL_SYMBOL : 'Symbol'
         }
-        
+
         self._results = []
         for provider in providers:
             self._results += [(provider, event) for event in provider.events]
-            
+
         self._row_count = len(self._results)
 
     def flags(self, index):
@@ -578,7 +578,7 @@ class ETWResultsModel(QtCore.QAbstractTableModel):
             # grab for speed
             row = index.row()
             column = index.column()
-            
+
             if column == ETWResultsModel.COL_GUID:
                 return "{%s}"%(self._results[row][0].guid)
             elif column == ETWResultsModel.COL_ID:
@@ -597,7 +597,7 @@ class ETWResultsModel(QtCore.QAbstractTableModel):
 
         # unhandeled request, nothing to do
         return None
-        
+
 
 class ETWResultsForm(idaapi.PluginForm):
     """
@@ -607,7 +607,7 @@ class ETWResultsForm(idaapi.PluginForm):
 
         super().__init__()
         self.providers = providers
-    
+
     def OnCreate(self, form):
         """
         Initialize the custom PyQt5 content on form creation.
@@ -620,10 +620,10 @@ class ETWResultsForm(idaapi.PluginForm):
         """
         Make the created form visible as a tabbed view.
         """
-        flags = idaapi.PluginForm.WOPN_TAB | idaapi.PluginForm.WOPN_PERSIST 
+        flags = idaapi.PluginForm.WOPN_TAB | idaapi.PluginForm.WOPN_PERSIST
         return idaapi.PluginForm.Show(self, "ETW", flags)
 
-    
+
     def _init_ui(self):
         """
         Init ui of ETW table
@@ -655,7 +655,7 @@ class ETWResultsForm(idaapi.PluginForm):
         vh.hide()
 
         # Allow multiline cells
-        self._table.setWordWrap(True) 
+        self._table.setWordWrap(True)
         self._table.setTextElideMode(QtCore.Qt.ElideMiddle);
         self._table.resizeColumnsToContents()
         self._table.resizeRowsToContents()
@@ -668,7 +668,7 @@ class ETWResultsForm(idaapi.PluginForm):
         """
         When user click on an event
         we send to windbg a special crafted debug command
-        
+
         That will set a conditional breakpoint on ntdll!EtwEventWrite function
         with condition on function argument that match the eventid and the provider GUID
         """
@@ -676,7 +676,7 @@ class ETWResultsForm(idaapi.PluginForm):
         guid = self._model._results[index.row()][0].guid
         delete_breakpoint("ntdll_EtwEventWrite")
         add_breakpoint(guid, event)
-        
-            
-def PLUGIN_ENTRY(): 
+
+
+def PLUGIN_ENTRY():
     return ETWBreaker()
